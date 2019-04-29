8f55a429ba1e94e114f3215d747d07b43b0a0728
Celia Concannon <celiaconcannon@dhcp-10-250-200-251.harvard.edu>
2019-04-29 17:09:07 -0400

    4/29 5pm commit

--- App/app.R
+++ App/app.R
@@ -4,8 +4,8 @@ library(janitor)
 library(ggpmisc) 
 library(lubridate)
 library(plotly)
+library(DT)
 
-Sys.setlocale("LC_ALL", "en_US.UTF-8")
 
 tesla <- read_csv("Tesla.csv - Tesla.csv.csv") %>%
   clean_names() %>%
@@ -13,45 +13,40 @@ tesla <- read_csv("Tesla.csv - Tesla.csv.csv") %>%
   filter(date > "2016-02-08") %>%
   filter(date < "2017-09-29")
 
-tweets <- read_csv("data_elonmusk.csv", locale = locale(encoding = "UTF-8")) %>%
+tweets <- read_csv("data_elonmusk.csv") %>%
   clean_names() %>%
-  mutate(time = as.Date(strptime(time, '%Y-%m-%d %H:%M:%S'),format = '%Y-%m-%d')) %>%
+  mutate(time = as.Date(strptime(time, '%Y-%m-%d %H:%M'),format = '%Y-%m-%d')) %>%
   filter(time > "2015-02-08") %>%
   filter(time < "2017-09-29") %>%
   rename(date = time)
 
 join <- left_join(tesla, tweets, by = "date") %>%
-  select(date, open, close, volume, tweet)
+  select(date, open, close, volume, tweet) %>%
+  mutate(tweetDisplay = paste("tweet:", tweet))
 
 
-ui <- basicPage(
-  titlePanel("Spikes in Tesla Stock Volume"),
-  plotlyOutput("plot1"),
-  verbatimTextOutput("info")
+ui <- fluidPage(
+  titlePanel("Elon Musk and Tesla Stock"),
+  verbatimTextOutput("info"),
   
-  #mainPanel(
-    
-    # Output: Tabset w/ plot, summary, and table ----
-    #tabsetPanel(type = "tabs",
-                #tabPanel("Stock Volume", plotOutput("volume")),
-                #tabPanel("Tweets", verbatimTextOutput("tweets"))
-# I plan to create tabs next, still have to figure out how to make the plot that will return which value you click within a tab.
+  mainPanel(
+    tabsetPanel(type = "tabs",
+                tabPanel("Plot", plotlyOutput("plot1")),
+                tabPanel("Summary", verbatimTextOutput("summary"))))
 )
 
 server <- function(input, output) {
   output$plot1 <- renderPlotly({
     
     p <- join %>%
-      select(date, volume, tweet) %>%
+      select(date, volume, tweet, tweetDisplay) %>%
       filter(!is.na(tweet)) %>%
-      ggplot(as.numeric = FALSE, aes(x = date, y = volume, text = paste("tweet:", tweet))) +
+      ggplot(aes(x = date, y = volume, text = tweetDisplay)) +
+      geom_point() +
       geom_line() +
       scale_y_continuous(labels = scales::comma) +
       labs(x = "Date",
-           y = "Volume") +
-      stat_peaks(colour = "red") +
-      stat_peaks(geom = "rug", colour = "red", sides = "b") +
-      expand_limits(y = 80) 
+           y = "Volume") 
     
     p %>%
       ggplotly()
