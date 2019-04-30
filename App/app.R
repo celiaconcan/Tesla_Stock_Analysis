c0f83a2b67cf5fe8524adb88ef12d1ac79ae982a
Celia Concannon <celiaconcannon@Celias-MacBook-Pro-2.local>
2019-04-30 12:51:40 -0400

    tuesday 1pm commit

--- App/app.R
+++ App/app.R
@@ -13,6 +13,9 @@ tesla <- read_csv("Tesla.csv - Tesla.csv.csv") %>%
   filter(date > "2016-02-08") %>%
   filter(date < "2017-09-29")
 
+# I picked this date range as a reasonale amount of time to display on the
+# x-axis of the graph without it being cluttered with two many points.
+
 tweets <- read_csv("data_elonmusk.csv") %>%
   clean_names() %>%
   mutate(time = as.Date(strptime(time, '%Y-%m-%d %H:%M'),format = '%Y-%m-%d')) %>%
@@ -21,13 +24,20 @@ tweets <- read_csv("data_elonmusk.csv") %>%
   rename(date = time) %>%
   select(date, tweet)
 
+# Had to clean, names, get the date columns to match and to be in the same date
+# range so I could join the two.
+
 join <- left_join(tesla, tweets, by = "date") %>%
   select(date, open, close, volume, tweet) %>%
   mutate(tweetDisplay = paste("tweet:", tweet))
 
+# Joins the two data sets by date and creates a new variable called tweetDisplay
+# which I will use in my plotly hover feature.
 
 ui <- fluidPage(
   titlePanel("Elon Musk and Tesla Stock"),
+  
+# Gives a title to the app.  
   
   mainPanel(
     tabsetPanel(type = "tabs",
@@ -36,6 +46,9 @@ ui <- fluidPage(
                 tabPanel("About", htmlOutput("summary"))))
 )
 
+# Creates my tabs, I plan on creating additional tabs by the due date.  I also
+# want to show how tweets affect stock closing prices.
+
 server <- function(input, output) {
   output$plot1 <- renderPlotly({
     
@@ -43,9 +56,15 @@ server <- function(input, output) {
       select(date, volume, tweet, tweetDisplay) %>%
       filter(!is.na(tweet)) %>%
       ggplot(aes(x = date, y = volume, text = tweetDisplay)) +
-      geom_point() +
-      geom_line() +
+      geom_point(size = 0.7, color = 'red') +
+      geom_line(group = 1) +
+      
+# Without the group aesthetic the line does not show up.
+  
       scale_y_continuous(labels = scales::comma) +
+      
+# Takes the y-axis labels out of scientific notation.  
+  
       labs(x = "Date",
            y = "Volume") 
     
@@ -53,8 +72,9 @@ server <- function(input, output) {
       ggplotly()
     
 # This has the shiny app create my ggplot, which shows peaks in tesla stock
-# volume.
-    
+# volume. ggplotly() wraps the ggplot in a plotly and allows me to use the hover
+# function to see text for each point.  I would like to use similar code going
+# forward to create a similar graph but using closing prices instead of volume.
 
   })
   output$summary <- renderUI({
@@ -71,6 +91,8 @@ server <- function(input, output) {
     str10 <- paste("Kaggle.com")
     HTML(paste(h3(str1), p(str2), h3(str3), p(str4), h5(str5), p(str6), h5(str7), p(str8), h5(str9), p(str10)))}) 
   
+# This creates each line of text for my about page.  The last line formats the
+# size of the text with headers being larger.
   
   output$table <- DT::renderDataTable({
     DT::datatable(tweets)
